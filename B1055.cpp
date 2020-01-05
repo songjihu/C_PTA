c3f47020cbcba2f322605916e5d02b7f9a13d28d
Your Name <you@example.com>
2020-01-05 20:18:36 +0800

    B1055

--- B1055.cpp
+++ B1055.cpp
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <math.h>
 #include <algorithm>
 #include <vector>
@@ -13,18 +14,15 @@ struct stu {
 };
 
 int cmp(struct stu a, struct stu b) {  //从身高依次排序
-	if (a.height > b.height)
+	if (a.height != b.height)
 		return a.height > b.height;
 	else
-		if (a.height < b.height)
-			return a.height < b.height;
-		else
-			return strcmp(a.name, b.name) > 0;
+		return strcmp(a.name, b.name) < 0;
 }
 
 int main() {
 
-	int n = 0, k = 0, i = 0, j = 0, m = 0, left = 0, right = 0;
+	int n = 0, k = 0, i = 0, j = 0, m = 0, point = 0, flag = 0, count = 0;
 	vector<stu> v;
 	stu temp;
 	cin >> n >> k;
@@ -33,8 +31,23 @@ int main() {
 		scanf("%s%d", temp.name, &temp.height);
 		v.push_back(temp);
 	}
-	sort(v.begin(), v.end(), cmp);
-	for (i = 0; i < n; i++)
-		printf("%s%d\n", v[i].name, v[i].height);
+	sort(v.begin(), v.end(), cmp);//从高到低排序
+	/*for (i = 0; i < n; i++)
+		printf("%s%d\n", v[i].name, v[i].height);*/
+	m = n / k + n % k;//最后一排人数m
+	char a[10002][10];
+	for (i = 0; i < k; i++) {
+		//从最后一排开始，每次打印一排
+		flag = 1; point = m / 2 + 1;//开始放置的位置，即为中间位置
+		for (j = 0; j < m; j++) {
+			//放置m次 flag表示移动方式
+			strcpy(a[point - 1], v[count].name);
+			flag % 2 == 1 ? point -= flag : point += flag;
+			flag++; count++;
+		}
+		for (j = 0; j < m - 1; j++) printf("%s ", a[j]);
+		printf("%s\n", a[j]);//打印
+		m = n / k;//重新计算每排人数
+	}
 	return 0;
 }
